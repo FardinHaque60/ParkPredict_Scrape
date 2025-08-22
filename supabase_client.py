692fb9223ea8efebf89543af05a5a5be1a7ad5fb
Fardin Haque <fardinhaque60@gmail.com>
2025-08-21 19:15:17 -0700

    supabase client for writing to db

---
+++ supabase_client.py
@@ -0,0 +1,25 @@
+import os
+from supabase import create_client, Client
+from dotenv import load_dotenv
+
+# Load .env file
+load_dotenv()
+
+# Get environment variables
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_SERVICE_ROLE_KEY = os.getenv("SUPABASE_SECRET_KEY")
+
+# Initialize client
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY)
+
+def write_to_supabase(table, timestamp, garage, fullness, mock):
+
+    data = {
+        "timestamp": timestamp,
+        "garage": garage,
+        "fullness": fullness,
+    }
+    if (mock):
+        print(f"Mock insert into {table}: {data}")
+        return
+    supabase.table(table).insert(data).execute()
