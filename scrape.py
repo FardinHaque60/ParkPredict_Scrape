71c9d6753938aa590df0e6fa973f63afe8d029d1
Fardin Haque <fardinhaque60@gmail.com>
2025-09-02 08:50:40 -0700

    disable mock

--- scrape.py
+++ scrape.py
@@ -4,8 +4,8 @@ from supabase_client import clean_6_months_old
 import datetime
 
 def scrape():
-    timestamp = scrape_park_data(mock=True) # get the timestamp for which real data was scraped for
-    scrape_api_data(timestamp, mock=True)
+    timestamp = scrape_park_data(mock=False) # get the timestamp for which real data was scraped for
+    scrape_api_data(timestamp, mock=False)
     dt = datetime.datetime.strptime(timestamp, "%Y-%m-%d %I:%M:%S %p")
     if dt.weekday() == 3 and 6 <= dt.hour < 7:  # clean up is done on Thursdays between 6AM and 7AM
         clean_6_months_old()
